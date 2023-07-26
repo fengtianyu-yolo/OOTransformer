acdebf0fcd6549bf2ffed903545e78de5add5b63
fengtianyu <fengtianyu@zhangyue.com>
2023-07-26 14:38:53 +0800

    添加配色

--- OOTransformer/Classes/UIColor/UIColor+Transformer.swift
+++ OOTransformer/Classes/UIColor/UIColor+Transformer.swift
@@ -10,7 +10,15 @@ import Foundation
 public extension UIColor {
     
     public static let primaryColor = UIColor()
+    public static let focusColor = UIColor()
+    public static let warningColor = UIColor()
     
     public static let backgroundColor_Level1 = UIColor()
+    public static let backgroundColor_Level2 = UIColor()
+    public static let backgroundColor_Level3 = UIColor()
+    
+    public static let titleColor = UIColor()
+    public static let bodyColor = UIColor()
+    public static let infoColor = UIColor()
     
 }
